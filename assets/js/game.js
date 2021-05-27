4fe05c641ec3fa346f821a07288ae3d927cde726
Anthony Pena <apena5@alumni.jh.edu>
2021-05-27 17:12:40 -0400

    fight skip bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,21 +3,36 @@
 // console.log(enemyNames[0]);
 // console.log(enemyNames[3]);
 
+var fightOrSkip = function () {
+	var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+	promptFight = promptFight.toLowerCase();
+
+	if (promptFight === "" || promptFight === null) {
+		window.alert("You need to provide a valid anser! Please try again.");
+		return fightOrSkip()
+	}
+
+	// if player picks "skip" confirm and then stop the loop
+	if (promptFight === "skip" || promptFight === "SKIP") {
+		var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+		if (confirmSkip) {
+			window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+			playerInfo.money = Math.max(0, playerInfo.money - 10);
+			console.log("playerInfo.money", playerInfo.money);
+			return true;
+		}
+	}
+	return false;
+};
+
 var fight = function (enemy) {
 	// repeat and execute as long as the enemy-robot is alive
 	while (playerInfo.health > 0 && enemy.health > 0) {
-
-		var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-
-		if (promptFight === "skip" || promptFight === "SKIP") {
-			var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-			if (confirmSkip) {
-				window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-				playerInfo.money = Math.max(0, playerInfo.money - 10);
-				console.log("playerInfo.money", playerInfo.money);
-				break;
-			}
+		if (fightOrSkip()) {
+			// if true, leave fight by breaking loop
+			break;
 		}
 
 		var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
@@ -173,7 +188,7 @@ var getPlayerName = function () {
 	console.log("Your robot's name is " + name);
 	return name;
 
-}
+};
 
 var playerInfo = {
 	name: getPlayerName(),
